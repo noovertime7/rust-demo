d83d3a0feadf9791521efeb0852f0ea2bda5fae2
chenteng <cc17854330572@163.com>
2023-11-06 15:22:20 +0800

    add package-demo2

--- package-demo2/src/main.rs
+++ package-demo2/src/main.rs
@@ -1,6 +1,26 @@
+use std::collections::HashMap;
 use package_demo2::house::house::House;
 
 fn main() {
-   let h  = House::NewHouse(String::from("Hourse 1"));
-    h.getRoomName()
+    let mut  hourses = Vec::new();
+    hourses.push(String::from("1"));
+    hourses.push(String::from("2"));
+    hourses.push(String::from("3"));
+
+   let res =  hourses.pop();
+    match res {
+        Some(val) => println!("pop val = {}",val),
+        None => println!("None"),
+    }
+    let mut scores = HashMap::new();
+
+    for hourse in &hourses {
+        let h  = House::NewHouse(String::from(hourse));
+        h.getRoomName();
+        scores.insert(hourse,10);
+    }
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
 }
